3e237970c5eb52df6e385dd1637cc58213f2bcd4
Hamish Eisler <heisler3030@users.noreply.github.com>
2014-09-03 23:06:48 -0700

    working SIN derivation

--- lib/bitpay/client.rb
+++ lib/bitpay/client.rb
@@ -83,18 +83,6 @@ module BitPay
 
     end
 
-
-    ## Generates a SIN based on public and private keys
-    #  Can be passed public and private keys or will use pub/private key from file-system by default
-    #
-    def generate_sin_from_keypair(params={})
-    end
-
-    ## Generates a keypair and associated SIN
-    #
-    def generate_keypair_and_sin
-    end
-
 ##### COMPATIBILITY METHODS #####
 
     ## Provided for legacy compatibility with old library
@@ -108,6 +96,64 @@ module BitPay
     def post(path, params={})
       send_request("POST", path, 'merchant', params)
     end
+
+##### CLASS METHODS #####
+
+    def self.get_public_key
+    end
+
+    ## Generates a SIN from private key
+    def self.get_sin
+      #http://blog.bitpay.com/2014/07/01/bitauth-for-decentralized-authentication.html
+      #https://en.bitcoin.it/wiki/Identity_protocol_v1
+
+      # NOTE:  All Digests are calculated against the binary representation, 
+      # hence the requirement to use [].pack("H*")
+      
+      group = ECDSA::Group::Secp256k1
+      
+      #Generate Private Key
+      #private_key = 1 + SecureRandom.random_number(group.order - 1)
+      private_key = ENV["privkey"].to_i(16)      
+
+      #Generate Public Key
+      public_key = group.generator.multiply_by_scalar(private_key)
+      public_key_string_compressed = ECDSA::Format::PointOctetString.encode(public_key, compression:true)
+      puts "Public Key: #{public_key_string_compressed.unpack("H*").first}" 
+      
+      # Step 1: SHA-256 of Public Key
+      step_one = Digest::SHA256.hexdigest(public_key_string_compressed)  # Works when PK is in binary format
+      #puts "step_one: #{step_one}"
+
+      # Step 2: RIPEMD-160 of Step 1
+      step_two = Digest::RMD160.hexdigest([step_one].pack("H*")) 
+      #puts "step_two #{step_two}"
+
+      # Step 3: Version + SIN TYPE + Step 2
+      step_three = "0F02" + step_two
+      #puts "step_three: #{step_three}"
+
+      # Step 4: Double SHA-256 of Step 3
+      step_four = Digest::SHA256.hexdigest([Digest::SHA256.hexdigest([step_three].pack("H*"))].pack("H*"))
+      #puts "step_four: #{step_four}"
+
+      # Step 5: Checksum (first 8 chars)
+      step_five = step_four[0..7]
+      #puts "step_five: #{step_five}"
+
+      # Step 6: Step 3 + Step 5
+      step_six = step_three + step_five
+      #puts "step_six: #{step_six}"
+
+      # Step 7: Base58 Encode
+      step_seven = encode_base58(step_six)
+      #puts "step_seven: #{step_seven}"
+
+      # Return the SIN
+      return step_seven
+      
+    end
+
 
 ##### PRIVATE METHODS #####
     private
@@ -164,5 +210,32 @@ module BitPay
       return ECDSA::Format::SignatureDerString.encode(signature).unpack("H*").first
       end
     end
+
+    ## Base58 Encoding Method
+    #
+    def self.encode_base58 (data) 
+      code_string = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+      base = 58
+
+      x = data.hex
+      
+      output_string = ""
+
+      while x > 0 do
+        remainder = x % base
+        x = x / base
+        output_string << code_string[remainder]
+      end
+
+      pos = 0
+      
+      while data[pos,2] == "00" do
+        output_string << code_string[0]
+        pos += 2
+      end
+
+     output_string.reverse()
+    end
+
   end
 end
--- lib/harness.rb
+++ lib/harness.rb
@@ -1,32 +1,24 @@
 require_relative 'bitpay.rb'
 
-# From Bitpay Web generator
-# ENV["pubkey"] = "0320ded0ab599f3722425ec3f24bbc91e587dc93cb807b729a3e57a1527fcdf464"
-# ENV["privkey"] = "1f44e6a65f549c5e7efdda84af12480602e2926479ea1ebd19c8a7d55da218cc"
-# ENV['SIN'] = "TfDEenRq7nNqUECzCHFgL6y3mRnNYtLvo9E"
-# POS Token MRUUyvzUtMZq9R2YkZ2riM
+# Test SIN Generation class methods
 
-# From Dev Hipchat - gives "Invalid Signature"
-# ENV['SIN'] = "TfFVQhy2hQvchv4VVG4c7j4XPa2viJ9HrR8"
-# ENV["privkey"] = "16d7c3508ec59773e71ae728d29f41fcf5d1f380c379b99d68fa9f552ce3ebc3"
-# ENV["pubkey"] = "0353a036fb495c5846f26a3727a28198da8336ae4f5aaa09e24c14a4126b5d969d"
-# POS Token: 64Qpw6gN4rSKLJb1LsfuR2E2P7QgEqGWY78RczNZung6
+# Generate SIN
+ENV["privkey"] = "16d7c3508ec59773e71ae728d29f41fcf5d1f380c379b99d68fa9f552ce3ebc3"
+puts "privkey: #{ENV['privkey']}"
+puts "target SIN: TfFVQhy2hQvchv4VVG4c7j4XPa2viJ9HrR8"
+puts "Derived SIN: #{BitPay::Client.get_sin}"
 
-# From Node Client - Gives "Invalid Public Key"
-# ENV['SIN'] = "Tezeb3ToLu2tVnAhQED8FENDgVkHp4RKXBj"
-# ENV["privkey"] = "2CMdg5UtMhnGh4qcoerEeXegojbBkcojghPkxv9LiQjR89PkC71jKT8KE5crcyACjFngZskyCqtTv8eniWNGxyFiVd1vFnzC6DXSFLLSgFRPYc"
-# ENV["pubkey"] = "Tezeb3ToLu2tVnAhQED8FENDgVkHp4RKXBj"
+puts "\n\n------------------\n\n"
 
-# From Dev Hipchat - gives "Invalid Signature"
- ENV['SIN'] = "TfFVQhy2hQvchv4VVG4c7j4XPa2viJ9HrR8"
- ENV["privkey"] = "16d7c3508ec59773e71ae728d29f41fcf5d1f380c379b99d68fa9f552ce3ebc3"
- ENV["pubkey"] = "0353a036fb495c5846f26a3727a28198da8336ae4f5aaa09e24c14a4126b5d969d"
-# POS Token: 64Qpw6gN4rSKLJb1LsfuR2E2P7QgEqGWY78RczNZung6
+#### Test Invoice Creation using directly assigned keys 
+## (Ultimately pubkey and SIN should be derived)
 
-client = BitPay::Client.new({insecure: true, debug: true})
+ENV["privkey"] = "16d7c3508ec59773e71ae728d29f41fcf5d1f380c379b99d68fa9f552ce3ebc3"
+ENV["pubkey"] = "0353a036fb495c5846f26a3727a28198da8336ae4f5aaa09e24c14a4126b5d969d"
+ENV['SIN'] = "TfFVQhy2hQvchv4VVG4c7j4XPa2viJ9HrR8"
+
+client = BitPay::Client.new({insecure: true, debug: false})
 
 invoice = client.post 'invoices', {:price => 10.00, :currency => 'USD'}
 
 puts "Here's the invoice: \n" + JSON.pretty_generate(invoice)
-
-#client.get

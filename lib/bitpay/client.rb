6e80e47ee0003a6c981b7f6814a4ebee3a904e6f
Paul Daigle <paul@bitpay.com>
2014-12-09 16:26:58 -0500

    Added some error handling
    Connection Refused error when pairing
    Invalid pairing code if pairing code malformed

--- lib/bitpay/client.rb
+++ lib/bitpay/client.rb
@@ -40,6 +40,7 @@ module BitPay
     end
 
     def pair_pos_client(claimCode)
+      raise BitPay::ArgumentError, "pairing code is not legal" unless verify_claim_code(claimCode)
       response = set_pos_token(claimCode)
       get_token 'pos'
       response
@@ -104,7 +105,12 @@ module BitPay
       request['Content-Type'] = 'application/json'
       request['X-BitPay-Plugin-Info'] = 'Rubylib' + VERSION
 
-      response = @https.request request
+      begin
+        response = @https.request request
+      rescue => error
+        raise BitPay::ConnectionError, "#{error.message}"
+        binding.pry
+      end
 
       if response.kind_of? Net::HTTPSuccess
         return JSON.parse(response.body)
@@ -156,5 +162,10 @@ module BitPay
       token = @tokens[facade] || load_tokens[facade] || raise(BitPayError, "Not authorized for facade: #{facade}")
     end
 
+    def verify_claim_code(claim_code)
+      regex = /^[[:alnum:]]{7}$/
+      matches = regex.match(claim_code)
+      !(matches.nil?)
+    end
   end
 end

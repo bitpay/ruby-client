bitpay/ruby-client

commit 671fe5657b3558c242fa2367e979c3d9ca571b2b

Eliminate web driver steps from cucumber
remove capybara completely

undo version bump, there's nothing here but test changes

features/step_definitions/step_helpers.rb
@@ -1,10 +1,8 @@
-require 'capybara/poltergeist'
 require 'pry'
 require 'fileutils' 
 
 require File.join File.dirname(__FILE__), '..', '..', 'lib', 'bitpay_sdk.rb'
 require_relative '../../config/constants.rb'
-require_relative '../../config/capybara.rb'
 
 
 module BitPay
@@ -16,78 +14,18 @@ module BitPay
   TOKEN_FILE_PATH = File.join(BITPAY_CREDENTIALS_DIR, TOKEN_FILE)
 end
 
-# Lots of sleeps in here to deal with finicky transitions and PhantomJS
-def get_claim_code_from_server
-  Capybara::visit ROOT_ADDRESS
-  log_in unless logged_in
-  Capybara::visit DASHBOARD_URL
-  raise "Bad Login" unless Capybara.current_session.current_url == DASHBOARD_URL
-  Capybara::visit "#{ROOT_ADDRESS}/api-tokens"
-  Capybara::find(".token-access-new-button").find(".btn").find(".icon-plus", match: :first).trigger("click")
-  sleep 0.50
-  Capybara::find(".token-access-new-button-wrapper").find_by_id("token-new-form", visible: true).find(".btn").trigger("click")
-  Capybara::find(".token-claimcode", match: :first).text
-end
-
-def approve_token_on_server(pairing_code)
-  Capybara::visit ROOT_ADDRESS
-  log_in unless logged_in
-  Capybara::visit DASHBOARD_URL
-  raise "Bad Login" unless Capybara.current_session.current_url == DASHBOARD_URL
-  Capybara::visit "#{ROOT_ADDRESS}/api-tokens"
-  Capybara::fill_in 'pairingCode', :with => pairing_code
-  sleep 0.50
-  Capybara::find(".token-access-approve-pairing").find(".token-access-approve-pairing-code").find(".btn").trigger("click")
-  sleep 0.50
-  Capybara::find(".token-access-request-manager").find(".token-access-request").find(".api-manager-actions-confirm").find(".ajaxForm").find(".btn").trigger("click")
-end
-
-def log_in
-  Capybara::visit "#{ROOT_ADDRESS}/dashboard/login/"
-  Capybara::fill_in 'email', :with => TEST_USER
-  Capybara::fill_in 'password', :with => TEST_PASS
-  Capybara::click_on('Login')
-  Capybara::find(".ion-gear-a", match: :first)
-end
-
-def new_paired_client
-  claim_code = get_claim_code_from_server
-  pem = BitPay::KeyUtils.generate_pem
-  client = BitPay::SDK::Client.new(api_uri: ROOT_ADDRESS, pem: pem, insecure: true)
-  client.pair_pos_client(claim_code)
-  client
-end
-
 def new_client_from_stored_values
-  if File.file?(BitPay::PRIVATE_KEY_PATH) && File.file?(BitPay::TOKEN_FILE_PATH)
-    token = get_token_from_file
-    pem = File.read(BitPay::PRIVATE_KEY_PATH)
-    client = BitPay::SDK::Client.new(pem: pem, tokens: token, insecure: true, api_uri: ROOT_ADDRESS )
-    unless client.verify_tokens then 
-      raise "Locally stored tokens are invalid, please remove #{BitPay::TOKEN_FILE_PATH}" end
-  else
-    pem = BitPay::KeyUtils.generate_pem
-    client = BitPay::SDK::Client.new(api_uri: ROOT_ADDRESS, pem: pem, insecure: true)
-    sleep 1 # rate limit compliance
-    response = client.pair_client({facade: 'merchant'})
-    pairing_code = response.first["pairingCode"]
-    token = response  #.first["token"]
-    approve_token_on_server(pairing_code)
-    
-
-    FileUtils.mkdir_p(BitPay::BITPAY_CREDENTIALS_DIR)
-    File.write(BitPay::PRIVATE_KEY_PATH, pem)
-    File.write(BitPay::TOKEN_FILE_PATH, JSON.generate(token))
-  end   
-  client
+  pem = ENV['BITPAYPEM'].gsub("\\n", "\n")
+  BitPay::SDK::Client.new(api_uri: APIURI, pem: pem, insecure: true)
 end
 
-def get_token_from_file
-  token = JSON.parse(File.read(BitPay::TOKEN_FILE_PATH))[0]
-  {token['facade'] => token['token']}
+def get_claim_code_from_server client
+  token = client.get(path: "tokens")["data"].select{|tuple| tuple["merchant"]}.first.values.first
+  client.post(path: "tokens", token: token, params: {facade: "pos"})["data"][0]["pairingCode"]
 end
 
-def logged_in
-  Capybara::visit DASHBOARD_URL
-  Capybara::page.has_content?('Dashboard') #has_link?('Dashboard')
+def client_has_tokens client
+  data = client.get(path: "tokens")["data"]
+  data.select{|tuple| tuple["pos"]}.any? && data.select{|tuple| tuple["merchant"]}.any?
 end
+

.gitignore
@@ -10,3 +10,4 @@ pkg
 bitpaykey.pem
 constants.txt
 coverage/
+.pem.data

.travis.yml
@@ -1,2 +1,3 @@
+sudo: false
 rvm:
 - 2.1.0

Rakefile
@@ -1,12 +1,9 @@
 require "bundler/gem_tasks"
 require 'rspec/core/rake_task'
-require 'capybara'
-require 'capybara/poltergeist'
 require 'mongo'
 require 'cucumber'
 require 'cucumber/rake/task'
 require_relative 'config/constants.rb'
-require_relative 'config/capybara.rb'
 
 RSpec::Core::RakeTask.new(:spec)
 

bitpay-sdk.gemspec
@@ -26,9 +26,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'pry', '0.10.1'
   s.add_development_dependency 'pry-byebug', '2.0.0'
   s.add_development_dependency 'pry-rescue', '1.4.1'
-  s.add_development_dependency 'capybara', '2.4.3'
   s.add_development_dependency 'cucumber', '~> 1.3.17'
-  s.add_development_dependency 'poltergeist', '1.5.1'
   s.add_development_dependency 'airborne', '0.0.20'
   s.add_development_dependency 'rspec', '3.1.0'
   s.add_development_dependency 'mongo', '1.11.1'

config/capybara.rb
@@ -1,6 +1,0 @@
-Capybara.javascript_driver = :poltergeist
-Capybara.default_driver = :poltergeist
-Capybara.default_wait_time = 10
-Capybara.register_driver :poltergeist do |app|
-	Capybara::Poltergeist::Driver.new(app, timeout: 60, js_errors: false, phantomjs_options: ['--ignore-ssl-errors=yes', '--ssl-protocol=TLSv1', '--web-security=false'])
-end

config/constants.rb
@@ -5,19 +5,8 @@
 #    source ./spec/set_constants.sh https://test.bitpay.com testuser@gmail.com mypassword
 #
 
-ROOT_ADDRESS = ENV['RCROOTADDRESS']
-TEST_USER = ENV['RCTESTUSER']
-TEST_PASS = ENV['RCTESTPASSWORD']
-DASHBOARD_URL = "#{ROOT_ADDRESS}/dashboard/merchant/home"
+APIURI = ENV['BITPAYAPI']
 
 # Specify a bitpay txid which has 6+ confirmations.  Default belongs to 'bitpayrubyclient@gmail.com' test account
 REFUND_TRANSACTION = ENV['REFUND_TRANSACTION']
 REFUND_ADDRESS = ENV['REFUND_ADDRESS']
-
-unless
-  ROOT_ADDRESS &&
-  TEST_USER &&
-  TEST_PASS
-then
-  raise "Missing configuration options - see constants.rb"
-end

features/pairing.feature
@@ -5,11 +5,11 @@ Feature: pairing with bitpay
 
   Scenario: the client has a correct pairing code
     Given the user pairs with BitPay with a valid pairing code
-    Then the user is paired with BitPay
+    Then the user receives a require token from bitpay
 
   Scenario: the client initiates pairing
     Given the user performs a client-side pairing
-    Then the user has a merchant token
+    Then the user receives an inactive token from bitpay
 
   Scenario Outline: the client has a bad pairing code
     Given the user fails to pair with a semantically <valid> code <code>
@@ -17,8 +17,3 @@ Feature: pairing with bitpay
   Examples:
       | valid   | code       | error                 | message                       |
       | invalid | "a1b2c3d4" | BitPay::ArgumentError | "pairing code is not legal"   |
-
-  Scenario: the client has a bad port configuration to a closed port
-    When the fails to pair with BitPay because of an incorrect port
-    Then they will receive a BitPay::ConnectionError matching "Connection refused" 
-

commit 9e0466cb833152407bb712b739aa9ef531b9a0e5

Fix Travis failure on nil ENV['BITPAYPEM']
The commit 671fe5657b3558c242fa2367e979c3d9ca571b2b introduced (a new ENV['BITPAYPEM'])[https://github.com/bitpay/ruby-client/commit/671fe5657b3558c242fa2367e979c3d9ca571b2b#diff-2fbdf6e6f53a9bb9a2c06059507373a8R18] but it wasn't defined anywhere. There is already (a constant called `PEM` in the `spec_helper.rb` file)[https://github.com/bitpay/ruby-client/blob/master/spec/spec_helper.rb#L12] so figured it should just use that. This could be moved to the constants config file instead if preferred and use the ENV variable and default to a value if ENV variable is nil.

features/step_definitions/step_helpers.rb
@@ -15,7 +15,7 @@ module BitPay
 end
 
 def new_client_from_stored_values
-  pem = ENV['BITPAYPEM'].gsub("\\n", "\n")
+  pem = PEM.gsub("\\n", "\n")
   BitPay::SDK::Client.new(api_uri: APIURI, pem: pem, insecure: true)
 end
 
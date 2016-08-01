95d859b7efd51a9c1309f223e7ea65bcbca2e39a
Javier Julio <JJfutbol@gmail.com>
2016-08-01 13:11:55 -0400

    Add PEM constant for cucumber tests
    The tests failing are for cucumber so using the constant in step_helpers.rb didn't fix anything since we need it defined here. I saw it defined in spec_helper.rb but that's only for rspec tests.

--- config/constants.rb
+++ config/constants.rb
@@ -6,6 +6,7 @@
 #
 
 APIURI = ENV['BITPAYAPI']
+PEM = ENV['BITPAYPEM'] || "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEICg7E4NN53YkaWuAwpoqjfAofjzKI7Jq1f532dX+0O6QoAcGBSuBBAAK\noUQDQgAEjZcNa6Kdz6GQwXcUD9iJ+t1tJZCx7hpqBuJV2/IrQBfue8jh8H7Q/4vX\nfAArmNMaGotTpjdnymWlMfszzXJhlw==\n-----END EC PRIVATE KEY-----\n"
 
 # Specify a bitpay txid which has 6+ confirmations.  Default belongs to 'bitpayrubyclient@gmail.com' test account
 REFUND_TRANSACTION = ENV['REFUND_TRANSACTION']
